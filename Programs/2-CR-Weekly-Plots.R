d262052eae2463c61f5a2629f9a60387be701093
Meng-Hsuan Hsieh <rexhsieh@Meng-Hsuans-MacBook-Air.local>
2022-10-01 15:09:00 -0400

    init

--- Programs/2-CR-Weekly-Plots.R
+++ Programs/2-CR-Weekly-Plots.R
@@ -11,33 +11,21 @@ options(scipen=999)
 # set seed for pilot
 set.seed(3333)
 
-pacman::p_load(ramify, fastDummies, doParallel, foreach, data.table, nbpMatching, sandwich, lmtest, haven, tidyverse, xtable)
-
-registerDoParallel(cores = 36)
-getDoParWorkers()
-getDoParName()
+pacman::p_load(data.table, sandwich, lmtest, haven, tidyverse, xtable)
 
 # working directory
-setwd("/Users/rexhsieh/Dropbox (University of Michigan)/Attrition RCT - Applications/2. Casaburi & Reed (2021)")
+setwd(dirname(dirname(rstudioapi::getSourceEditorContext()$path)))
 
 # data
-bags <- as.data.table(read_dta("R2/dta/data_for_analysis/data_bags_cleaned_210630.dta"))
-credit <- as.data.table(read_dta("R2/dta/data_for_analysis/data_credit_cleaned_210630.dta"))
-quantity <- as.data.table(read_dta("R2/dta/data_for_analysis/data_quantity_weekly_cleaned_210630.dta"))
+bags <- as.data.table(read_dta("Data/data_bags_cleaned_210630.dta"))
 
-# df = gm_cleaned; Y = "admin_loanrenewal"; D = "treat"; A = "attrition"; G = "pair"   ##debug
-# reg pf treat i.pair 
-# table
-cr_attr_est <- data.table(reg = c("Bags",
-                                  "Credit",
-                                  "Quantity"),
-                          orig_est = NA_real_, theta_n = NA_real_, attrition = NA_real_)
-
+# unique weeks in data
 weeks <- unique(bags[after==1][a16_1_gradeA==1]$week)
 
-# (1) ----------
+# results list initialization
 results <- list()
 
+# regression for every week
 for (w in weeks) {
   
   results[[w]] <- list(as.numeric(lm(pf ~ treat + factor(pair), data = bags[week==w][after==1][a16_1_gradeA==1])$coefficients[2]),
@@ -45,19 +33,22 @@ for (w in weeks) {
   
 }
 
+# results data frame
 results_dt <- rbindlist(results)
 results_dt[, week := paste0(1960+floor(weeks/52), "w", weeks%%52)]
+
+# relabel and reformat things
 setorder(results_dt, "week")
 setnames(results_dt, c("V1","V2"), c("With Pair Fixed Effects", "Without Pair Fixed Effects"))
 results_dt_w <- melt(results_dt)
 
-# calculation----
+# deviation from original estimates calculations
 results_dt[, `(wo pfe - pfe) / pfe` := abs((`Without Pair Fixed Effects` - `With Pair Fixed Effects`) / abs(`With Pair Fixed Effects`)) * 100
            ][, `(pfe - wo pfe) / wo pfe` := abs((`With Pair Fixed Effects` - `Without Pair Fixed Effects`) / abs(`Without Pair Fixed Effects`)) * 100]
 
 mean(results_dt$`(pfe - wo pfe) / wo pfe`, na.rm=TRUE)
 
-# graph ------
+# point estimates plot by week ------
 p <- ggplot(results_dt_w) +
   geom_point(aes(x=week, y=value, shape=variable, color=variable), size = 3) +
   xlab("Week Number") +
@@ -76,32 +67,6 @@ p <- ggplot(results_dt_w) +
 p + scale_colour_discrete(labels = function(x) str_wrap(x, width = 6)) + scale_shape_discrete(labels = function(x) str_wrap(x, width = 6)) +
   scale_y_continuous(breaks = round(seq(-200, 100, by = 50),1))
 
-ggsave("CR Point Estimates by Week (1).jpg")
+# export weekly plot
+ggsave("Output/CR Point Estimates by Week (1).jpg")
 
-
-# # (2) ----------
-# results <- list()
-# 
-# for (w in weeks) {
-#   
-#   t5_reg_5 <- lm(pf ~ treat + a6_weightA_B + villages_B + b_sup_sid_B + lshare_sid_B + b4_age_B   + b7_2_years_mid_B + nice_floor_B + mobile_B + storage_B  + factor(pair), data = bags[after==1][a16_1_gradeA==1][week==w])
-#   t5_reg_5$coefficients[2] 
-#   
-#   results[[w]] <- list(as.numeric(t5_reg_5$coefficients[2]),
-#                        as.numeric(lm(pf ~ treat + a6_weightA_B + villages_B + b_sup_sid_B + lshare_sid_B + b4_age_B + b7_2_years_mid_B + nice_floor_B + mobile_B + storage_B, data = bags[after==1][a16_1_gradeA==1][week==w])$coefficients[2]))
-#   
-# }
-# 
-# results_dt <- rbindlist(results)
-# results_dt[, week := paste0(1960+floor(weeks/52), "w", weeks%%52)]
-# setorder(results_dt, "week")
-# 
-# ggplot(results_dt) +
-#   geom_point(aes(x=week, y=V2), color = "red") +
-#   geom_point(aes(x=week, y=V1)) +
-#   xlab("Week Number") +
-#   ylab("Point Estimates") +
-#   theme_minimal()
-# 
-# ggsave("CR Point Estimates by Week (2).jpg")
-# 
--- Programs/2-CR-attrition-estimates.R
+++ Programs/2-CR-attrition-estimates.R
@@ -1,6 +1,6 @@
 ###############################
-# date: 9/1/2022
-# Groh & McKenzie application
+# date: 09/20/2022
+# Casaburi & Reed application
 ###############################
 
 rm(list = ls())
@@ -11,22 +11,14 @@ options(scipen=999)
 # set seed for pilot
 set.seed(3333)
 
-pacman::p_load(ramify, fastDummies, doParallel, foreach, data.table, nbpMatching, sandwich, lmtest, haven, tidyverse, xtable)
-
-registerDoParallel(cores = 36)
-getDoParWorkers()
-getDoParName()
+pacman::p_load(data.table, sandwich, lmtest, haven, tidyverse, xtable)
 
 # working directory
-setwd("/Users/rexhsieh/Dropbox (University of Michigan)/Attrition RCT - Applications/2. Casaburi & Reed (2021)")
+setwd(dirname(dirname(rstudioapi::getSourceEditorContext()$path)))
 
 # data
-bags <- as.data.table(read_dta("R2/dta/data_for_analysis/data_bags_cleaned_210630.dta"))
-credit <- as.data.table(read_dta("R2/dta/data_for_analysis/data_credit_cleaned_210630.dta"))
-quantity <- as.data.table(read_dta("R2/dta/data_for_analysis/data_quantity_weekly_cleaned_210630.dta"))
+bags <- as.data.table(read_dta("Data/data_bags_cleaned_210630.dta"))
 
-# df = gm_cleaned; Y = "admin_loanrenewal"; D = "treat"; A = "attrition"; G = "pair"   ##debug
-# reg pf treat i.pair 
 # table
 cr_attr_est <- data.table(reg = c("Bags (1)",
                                   "Bags (2)",
@@ -35,68 +27,75 @@ cr_attr_est <- data.table(reg = c("Bags (1)",
                                   "Bags (5)"),
                           Original = NA_real_, Theta = NA_real_, Attrition = NA_real_)
 
-# bags ----------
+# bags (1) ----------
 t5_reg <- lm(pf ~ treat + factor(pair), data = bags[after==1][a16_1_gradeA==1])
 t5_reg$coefficients[2]
 
+# save estimates
 cr_attr_est[1, 2:3] <- list(as.numeric(t5_reg$coefficients[2]),
                             as.numeric(lm(pf ~ treat, data = bags[after==1][a16_1_gradeA==1])$coefficients[2]))
+
+# calculate attrition rate
 cr_attr_est[1, 4] <- list(100 - 100 * length(unique(bags[after==1][a16_1_gradeA==1][is.na(pf)==FALSE]$sid)) / 80)
 
-# bags FE ----------
+# bags (2) FE ----------
 t5_reg <- lm(pf ~ treat + factor(week) + factor(pair), data = bags[after==1][a16_1_gradeA==1])
 t5_reg$coefficients[2]
 
+# save estimates
 cr_attr_est[2, 2:3] <- list(as.numeric(t5_reg$coefficients[2]),
                             as.numeric(lm(pf ~ treat + factor(week), data = bags[after==1][a16_1_gradeA==1])$coefficients[2]))
+
+# calculate attrition rate
 cr_attr_est[2, 4] <- list(100 - 100 * length(unique(bags[after==1][a16_1_gradeA==1][is.na(pf)==FALSE]$sid)) / 80)
 
-# bags - FE TC -----------
+# bags (3) FE TC -----------
 t5_reg_5 <- lm(pf ~ treat + a6_weightA_B + villages_B + b_sup_sid_B + lshare_sid_B + b4_age_B   + b7_2_years_mid_B + nice_floor_B + mobile_B + storage_B  + factor(week) + factor(pair), data = bags[after==1][a16_1_gradeA==1])
 t5_reg_5$coefficients[2]
 
-length(unique(bags[after==1][a16_1_gradeA==1][!(is.na(a6_weightA_B) | is.na(villages_B) | is.na(b_sup_sid_B) | is.na(lshare_sid_B) | is.na(b4_age_B) | is.na(b7_2_years_mid_B) | is.na(nice_floor_B) | is.na(mobile_B) | is.na(storage_B))]$sid))
-
+# save estimates
 cr_attr_est[3, 2:3] <- list(as.numeric(t5_reg_5$coefficients[2]),
                             as.numeric(lm(pf ~ treat + a6_weightA_B + villages_B + b_sup_sid_B + lshare_sid_B + b4_age_B + b7_2_years_mid_B + nice_floor_B + mobile_B + storage_B + factor(week), data = bags[after==1][a16_1_gradeA==1])$coefficients[2]))
+
+# calculate attrition rate
 cr_attr_est[3, 4] <- list(100 - 100 * length(unique(bags[after==1][a16_1_gradeA==1][is.na(pf)==FALSE]$sid)) / 80)
 
-# bags - FE VC -----------
+# bags (4) FE VC -----------
 t5_reg_5 <- lm(pf ~ treat + lshare + N_traders_oth + N_treat_oth + milesnearest + suppliers + chief_1 + chief_2 + chief_3 + chief_4 + chief_5 + factor(week) + factor(pair), data = bags[after==1][a16_1_gradeA==1])
 t5_reg_5$coefficients[2] 
 
+# save estimates
 cr_attr_est[4, 2:3] <- list(as.numeric(t5_reg_5$coefficients[2]),
                             as.numeric(lm(pf ~ treat + lshare + N_traders_oth + N_treat_oth + milesnearest + suppliers + chief_1 + chief_2 + chief_3 + chief_4 + chief_5 + factor(week), data = bags[after==1][a16_1_gradeA==1])$coefficients[2]))
+
+# calculate attrition rate
 cr_attr_est[4, 4] <- list(100 - 100 * length(unique(bags[after==1][a16_1_gradeA==1][is.na(pf)==FALSE]$sid)) / 80)
 
-#  bags - everything ----------
+#  bags (%) everything ----------
 t5_reg_5 <- lm(pf ~ treat + a6_weightA_B + villages_B + b_sup_sid_B + lshare_sid_B + b4_age_B   + b7_2_years_mid_B + nice_floor_B + mobile_B + storage_B + lshare + N_traders_oth + N_treat_oth + milesnearest + suppliers + chief_1 + chief_2 + chief_3 + chief_4 + chief_5 + factor(week) + factor(pair), data = bags[after==1][a16_1_gradeA==1])
 t5_reg_5$coefficients[2] 
 
+# save estimates
 cr_attr_est[5, 2:3] <- list(as.numeric(t5_reg_5$coefficients[2]),
                             as.numeric(lm(pf ~ treat + a6_weightA_B + villages_B + b_sup_sid_B + lshare_sid_B + b4_age_B   + b7_2_years_mid_B + nice_floor_B + mobile_B + storage_B + lshare + N_traders_oth + N_treat_oth + milesnearest + suppliers + chief_1 + chief_2 + chief_3 + chief_4 + chief_5 + factor(week), data = bags[after==1][a16_1_gradeA==1])$coefficients[2]))
+
+# calculate attrition rate
 cr_attr_est[5, 4] <- list(100 - 100 * length(unique(bags[after==1][a16_1_gradeA==1][is.na(pf)==FALSE]$sid)) / 80)
 
-# final table making -------------
-final_table <- dcast(melt.data.table(copy(cr_attr_est)), variable ~ reg) #.GRP, by = .(reg) # [,-c("Attrition")]
+# final table -------------
+final_table <- dcast(melt.data.table(copy(cr_attr_est)), variable ~ reg)
 
-# percentage deviatio ---------------
+# percentage deviation ---------------
 cr_attr_est[, ss := ifelse(reg=="Bags", 0, 1)
-][, pct := abs((Original - Theta)/abs(Theta)) * 100
-][, avg_pct_chg := mean(pct)
-  ][, pct_1 := abs((Theta - Original)/abs(Original)) * 100]
-
+            ][, pct := abs((Original - Theta)/abs(Theta)) * 100
+              ][, avg_pct_chg := mean(pct)
+                ][, pct_1 := abs((Theta - Original)/abs(Original)) * 100]
 
 # export ----------
-comment <- list(pos = list(0), command = NULL)
-comment$pos[[1]] <- c(nrow(cr_attr_est))
-comment$command <- c(paste("\\hline\n",
-                           "{\\footnotesize Note: For each outcome regression specification, we report (1) the point estimates reported in the original paper, (2) the standard difference-in-means estimators (\\eqref{}), (3) the estimator on the coefficient of treatment status with matched-pair/strata fixed effects, and (4) the attrition rate in \\% by outcome, defined as [number of matched-pairs/individuals with missing outcome in row/total number of matched-pairs/individuals]. Rows are labeled according to the outcome used in paper. Columns are labeled according to the types of estimates and attrition rate. The definitions of the outcomes can be found in the main text.}\n", sep = ""))
-
-fwrite(final_table, "attrition-est-results-final.csv")
-print(xtable(final_table, include.colnames=TRUE, booktabs=TRUE, type = "latex", digits = 5, caption = "Average Treatment Effect Estimates in Groh \\& McKenzie (2016)"),
+fwrite(final_table, "Output/CR-attrition-est-results-final.csv")
+print(xtable(final_table, include.colnames=TRUE, booktabs=TRUE, type = "latex", digits = 5, caption = "Average Treatment Effect Estimates in Casabury \\& Reed (2021)"),
       include.rownames=FALSE,
       # add.to.row = comment, 
       hline.after = c(-1,0),
-      file = "attrition-est-results-final.tex")
+      file = "Output/CR-attrition-est-results-final.tex")
 
